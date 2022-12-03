c51bd36401730fa00af89f4175ab2b6726808cd2
Yuliia0000 <migrishina0902@gmail.com>
2022-12-03 16:38:24 +0100

    first commit

---
+++ news/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(TranslationAdmin):
+    list_display = ('title', 'about', 'content', 'publish', 'source')
+    list_display_links = ('title', )
+    
+
+
